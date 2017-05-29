c8ec01e34ddc3c25c840aaad50ea9046ee20ef6d
blyzer <bffa918ec7bcd131d691264788e0119779a9bdee>
2017-05-28 23:23:33 -0400

    fiecd

--- src/Store.API/Startup.cs
+++ src/Store.API/Startup.cs
@@ -18,8 +18,8 @@ namespace Store.API
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
